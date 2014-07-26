940b85f86852439232717121acda31401539e8c3
Eliu Gonzalez Hernandez <elgoher@gmail.com>
2014-07-25 21:25:26 -0500

    resubidos
    archivos resubidos a github

---
+++ app.php
@@ -0,0 +1,30 @@
+<?php
+
+//metodo donde esta recibe el header, el footer y en el archivo target
+function insertar($headerUrl,$footerUrl,$targetUrl)
+{
+	$header = file_get_contents($headerUrl); //cargo el archivo header.html
+	$footer = file_get_contents($footerUrl); //cargo el archivo footer.html
+	$target = file_get_contents($targetUrl); //cargo el archivo target.html
+
+	$target = str_ireplace('<header>', $header, $target); // se inserta el header en target.html
+	$target = str_ireplace("<footer>", $footer, $target); // se inserta el footer en target.html
+
+	file_put_contents($targetUrl,$target);//se guarda el archivo modificado
+	return true;
+}
+//metodo que reinicia el el header, el footer y en el archivo target
+function reiniciar($headerUrl,$footerUrl,$targetUrl){
+
+	$header = file_get_contents($headerUrl); //cargo el archivo header.html
+	$footer = file_get_contents($footerUrl); //cargo el archivo footer.html
+	$target = file_get_contents($targetUrl); //cargo el archivo target.html
+
+	$target = str_ireplace($header,'<header>', $target); // se inserta el header en target.html
+	$target = str_ireplace($footer,"<footer>", $target); // se inserta el footer en target.html
+
+	file_put_contents($targetUrl,$target);//se guarda el archivo modificado
+	return true;
+}
+
+?>
---
+++ controller.php
@@ -0,0 +1,20 @@
+<?php
+include_once 'app.php';
+	$accion=$_GET["accion"] ;
+	if ($accion=="reset") {
+		$exito= reiniciar("header.html","footer.html","target.html");
+
+		if($exito)
+			header("Location: index.html");//se redirecciona a la url
+		else
+			print("algo salio mal");
+	}else{
+		$exito= insertar("header.html","footer.html","target.html");
+
+		if($exito)
+			header("Location: index.html");//se redirecciona a la url
+		else
+			print("algo salio mal");
+
+	}
+?>
